ab9e05c0ba253536a2cf0f622383ce03881b9976
David Kennedy <davek1802@gmail.com>
2018-10-12 11:49:58 -0700

    add filter and draw test specs

--- src/app/draw.service.spec.ts
+++ src/app/draw.service.spec.ts
@@ -1,12 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DrawService } from './draw.service';
+import { Card } from './Card';
+import { Observable, of } from 'rxjs';
 
 describe('DrawService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let drawService: DrawService;
+  let drawResponse: Card[] = [];
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [DrawService]
+    })
+    drawService = TestBed.get(DrawService);
+  });
 
   it('should be created', () => {
-    const service: DrawService = TestBed.get(DrawService);
-    expect(service).toBeTruthy();
+    expect(drawService).toBeTruthy();
+  });
+
+  // Add tests for getDraw() method
+  describe('getDraw', () => {
+    it('should return an Observable<Card[]>', () => {
+
+      let responseSize: Number = 52;
+      let response: Card[];
+
+      let card = new Card(0, 0, 12);
+      drawResponse.push(card);
+      //responseSize = 1;
+      //spyOn(drawService, 'getDraw').and.returnValue(of(drawResponse));
+
+      drawService.getDraw()
+        .subscribe(res => response = res);
+
+      expect(response.length).toEqual(responseSize);
+    });
+
   });
 });
--- src/app/filter.service.spec.ts
+++ src/app/filter.service.spec.ts
@@ -1,12 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FilterService } from './filter.service';
+import { Filter } from './filter';
+import { of } from 'rxjs';
 
 describe('FilterService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let filterService: FilterService;
+  let filterResponse: Filter = new Filter();
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [FilterService]
+    });
+
+    filterService = TestBed.get(FilterService);
+  });
 
   it('should be created', () => {
-    const service: FilterService = TestBed.get(FilterService);
-    expect(service).toBeTruthy();
+    expect(filterService).toBeTruthy();
+  });
+
+  // Add tests for getFilter() method
+  describe('getFilter', () => {
+    it('should return an Observable<Filter>', () => {
+      filterResponse.ndeck = 1;
+      filterResponse.suits = [0, 1, 2, 3];    // clubs ...
+      filterResponse.size= 5;
+      filterResponse.rank = { max: 12, min: 0 };
+      
+      let response: Filter;
+      //spyOn(filterService, 'getFilter').and.returnValue(of(filterResponse));
+
+      filterService.getFilter()
+        .subscribe(res => response = res);
+
+      expect(response).toEqual(filterResponse);
+    });
   });
 });
